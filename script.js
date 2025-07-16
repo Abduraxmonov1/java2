d9069435eb72976accbb87918ca7d1f88545fbee
Abduraxmonov1 <abubakir7083@gmail.com>
2025-07-16 15:06:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let a = prompt('your name')
+
+let b = +prompt('your number')
+
+let c = confirm('tasdiqlash')
+
+alert(`birinchi malumot: ${typeof a} ikkinchi: ${typeof c} uchinchi: ${typeof b}`)
